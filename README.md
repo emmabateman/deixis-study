f6092102bf654364ede1c65b986134980c087868
emmabateman <emmalbateman@gmail.com>
2019-12-13 19:33:53 -0800

    finished project

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # deixis-study
-a computational study of the distribution of the pronouns "this" and "that" in podcast transcripts
+a computational study of the distribution of the pronouns "this" and "that" in podcast transcripts.
+
+This project was developed on Ubuntu 18.04.3 with Python 3.6.9
+
+#data
+The data comes from 4 episodes of _My Brother, My Brother, and Me_ (episodes 481-484). The transcripts were posted on the Maximum Fun website in PDF format. Raw text was obtained by copy+pasting from the PDFs.
+
+clean\_data.py cleans the data by removing file headers and incorrectly copied characters.
+
+parse.py uses the Stanford NLP parser to produce syntax trees for each sentence.
+
+label\_tokens.py automatically marks demonstrative pronouns.
+
+After running the first three scripts, we labeled the referent for each pronoun by hand.
+
+features.py produces feature vectors for each labeled instance. These feature vectors are stored in the features folder
+
+split\_data.py randomly divides the feature vectors into test, dev, and train sets.
+
+#analysis
+
+Analysis of the data is performed by running analysis.py.
+
+As part of the analysis, 3 prediction models are trained using scikit.
+
+To run predictions on the test set instead of the dev set, use option -t.
+
+_This_ and _is_ can't be contracted due to phonological properties. This means that "next:is" correlates strongly with _this_ and "next:'s" correlates strongly with _that_. This is a problem if we want to discover patterns based on the semantic differences between _this_ and _that_ rather than their phonological differences.
+
+To eliminate the distinction between 's and is, use option -s.
--- analyze.py
+++ analyze.py
@@ -1,10 +1,12 @@
 import os
+import re
 import argparse
 import numpy as np
 from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
 
 feature_list = []
 feature_dict = {}
@@ -15,7 +17,7 @@ def confusion(true, pred):
     for i in range(len(error_vals)):
         if error_vals[i]:
             error_indices.append(i)
-    print("incorrect prediction on the following training cases: {0}".format(error_indices))
+    print("incorrect prediction on the following test cases: {0}".format(error_indices))
 
     matrix = confusion_matrix(true, pred)
     true_this = matrix[0][0]
@@ -44,21 +46,23 @@ def confusion(true, pred):
     print("\n")
 
 def nn(train_features, train_classes, test_features, test_classes):
-    classifier = MLPClassifier(hidden_layer_sizes=(10, 10))
-    print("running multi-layer perceptron with 2 hidden layers of 10 neurons")
+    layers = 2
+
+    classifier = MLPClassifier(hidden_layer_sizes=tuple([10 for i in range(layers)]))
+    print("running multi-layer perceptron with {0} hidden layers of 10 neurons".format(layers))
     classifier.fit(train_features, train_classes)
     predictions = classifier.predict(test_features)
     confusion(test_classes, predictions)
 
 def svm(train_features, train_classes, test_features, test_classes):
     classifier = SVC(kernel="linear")
-    print("running state vector machine algorithm with linear kernel")
+    print("running support-vector machine algorithm with linear kernel")
     classifier.fit(train_features, train_classes)
     predictions = classifier.predict(test_features)
     confusion(test_classes, predictions)
 
 def kneighbors(train_features, train_classes, test_features, test_classes):
-    n = 3
+    n = 5
     classifier = KNeighborsClassifier(n_neighbors=n)
     print("performing k-nearest-neighbor algorithm with {0} neighbors".format(n))
     classifier.fit(train_features, train_classes)
@@ -77,9 +81,7 @@ def get_features(lines):
         feat_list = line.split()[1:]
         for feat in feat_list:
             name, value = feat.split(':',1)
-            try:
-                value = int(value)
-            except:
+            if name != 'dist':
                 name = feat
             if not name in feature_list:
                 feature_dict[name] = len(feature_list)
@@ -97,10 +99,10 @@ def get_vectors(lines):
         feat_list = line.split()[1:]
         for feat in feat_list:
             name, value = feat.split(':', 1)
-            try:
+            if name == 'dist':
                 value = int(value)
                 feat_idx = feature_dict[name]
-            except:
+            else:
                 value = 1
                 if feat in feature_list:
                     feat_idx = feature_dict[feat]
@@ -114,6 +116,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--test", help="use the test set instead of the dev set", action="store_true")
+    parser.add_argument("-s", help="replace feature next1='s with next1=is", action="store_true")
     args = parser.parse_args()
     if args.test:
         print("FINAL TEST!")
@@ -126,6 +129,13 @@ def main():
     f2 = open(test_dir + "data.txt")
     train_lines = f1.readlines()
     test_lines = f2.readlines()
+
+    if args.s:
+        for i in range(len(train_lines)):
+            train_lines[i] = re.sub('next1:\'s', 'next1:is', train_lines[i])
+        for i in range(len(test_lines)):
+            test_lines[i] = re.sub('next1:\'s', 'next1:is', test_lines[i])
+
     get_features(train_lines)
     train_features, train_classes = get_vectors(train_lines)
     test_features, test_classes = get_vectors(test_lines)
@@ -134,7 +144,7 @@ def main():
     total = len(train_classes)
     num_that = sum(train_classes)
     num_this = total - num_that
-    num_ref = sum(train_features[:,feature_dict['ref']])
+    num_ref = sum(train_features[:,feature_dict['ref:1']])
     num_noref = total - num_ref
     print("The training data contains {0} 'this' pronouns ({1}%) and {2} 'that' pronouns ({3}%)".format(num_this, num_this*100/total, num_that, num_that*100/total))
     print("{0} of the pronouns ({1}%) have a referent in the text".format(num_ref, num_ref*100/total))
@@ -146,13 +156,30 @@ def main():
     nn(train_features, train_classes, test_features, test_classes)
 
     #covariance
-    print("TOP 10 FEATURES\n")
+    print("MOST PREDICTIVE FEATURES\n")
     correlation = [np.corrcoef(train_classes, train_features[:,i])[0,1] for i in range(len(feature_list))]
 
     correlation_dict = dict(zip(feature_list, correlation))
     sorted_features = sorted(correlation_dict.items(), key=lambda x: 1/abs(x[1]))
 
-    for tup in sorted_features[:10]:
-        print("{0}{1}{2}\t{3}".format(tup[0], ' '.join(['' for i in range(20 - len(tup[0]))]), abs(tup[1]), ['this', 'that'][tup[1]>0]))
+    for tup in sorted_features:
+        if abs(tup[1]) > 0.1:
+            print("{0}{1}\t{2}\t{3}".format(tup[0], ' '.join(['' for i in range(20 - len(tup[0]))]), ['this', 'that'][tup[1]>0], abs(tup[1])))
+
+    print("\n\n")
+
+    #analyze distance
+    dist_idx = feature_list.index('dist')
+    cat_idx = feature_list.index('cataphora:1')
+    distances = train_features[:,dist_idx] * (train_features[:,cat_idx] * -2 + 1)
+    avg_that = sum(distances * train_classes)/len(train_classes)
+    avg_this = sum(distances * (train_classes - 1)*-1)/len(train_classes)
+    print("Average distance for 'that':{0}\nAverage distance for 'this':{1}".format(avg_that, avg_this))
+    avg_that = sum(train_features[:,dist_idx] * train_classes)/len(train_classes)
+    avg_this = sum(train_features[:,dist_idx] * (train_classes - 1)*-1)/len(train_classes)
+    print("Average magnitude of distance for 'that':{0}\nAverage magnitude of distance for 'this':{1}".format(avg_that, avg_this))
+
+
+    #plt.show()
 
 main()
